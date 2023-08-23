18eee49a752052063aee7e595fa7b9c793450e71
Neto <joaonetoprojetos14@gmail.com>
2023-08-22 22:24:52 -0300

    Update breastdatabase.py

--- breastdatabase.py
+++ breastdatabase.py
@@ -8,44 +8,6 @@ data = load_breast_cancer()
 df = pd.DataFrame(data.data, columns=data.feature_names)
 df['target'] = data.target
 
-def show_correlation_heatmap():
-    plt.figure(figsize=(12, 8))
-    correlation_matrix = df.corr()
-    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
-    plt.title("Heatmap (Correlação entre Features)")
-    plt.tight_layout()
-    plt.show()
-
-def show_histograms():
-    plt.figure(figsize=(12, 10))
-    df.drop('target', axis=1).hist(bins=20, figsize=(12, 10))
-    plt.suptitle("Histogramas de cada Features Individualmente")
-    plt.tight_layout()
-    plt.show()
-
-def show_boxplot():
-    plt.figure(figsize=(12, 8))
-    sns.boxplot(data=df.drop('target', axis=1), orient="h")
-    plt.title("Boxplot de Todas as Features")
-    plt.xlabel("Valores das Features")
-    plt.tight_layout()
-    plt.show()
-
-def show_violin_plot():
-    plt.figure(figsize=(12, 8))
-    sns.violinplot(data=df.drop('target', axis=1), orient="h")
-    plt.title("Violin Plot de Todas as Features")
-    plt.xlabel("Valores das Features")
-    plt.tight_layout()
-    plt.show()
-
-def show_scatter_plot():
-    plt.figure(figsize=(12, 8))
-    sns.scatterplot(data=df, x='mean radius', y='mean texture', hue='target')
-    plt.title("Scatter Plot (Agrupado pelas Classes)")
-    plt.tight_layout()
-    plt.show()
-
 while True:
     print("\nMenu de Visualização:")
     print("1. Heatmap - Correlação entre todas as Features")
@@ -61,14 +23,52 @@ while True:
         print("Até mais.")
         break
     elif choice == '1':
+        def show_correlation_heatmap():
+            plt.figure(figsize=(12, 8))
+            correlation_matrix = df.corr()
+            sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
+            plt.title("Heatmap (Correlação entre Features)")
+            plt.tight_layout()
+            plt.show()
+            
         show_correlation_heatmap()
     elif choice == '2':
+        def show_histograms():
+            plt.figure(figsize=(12, 10))
+            df.drop('target', axis=1).hist(bins=20, figsize=(12, 10))
+            plt.suptitle("Histogramas de cada Features Individualmente")
+            plt.tight_layout()
+            plt.show()
+            
         show_histograms()
     elif choice == '3':
+        def show_boxplot():
+            plt.figure(figsize=(12, 8))
+            sns.boxplot(data=df.drop('target', axis=1), orient="h")
+            plt.title("Boxplot de Todas as Features")
+            plt.xlabel("Valores das Features")
+            plt.tight_layout()
+            plt.show()
+            
         show_boxplot()
     elif choice == '4':
+        def show_violin_plot():
+            plt.figure(figsize=(12, 8))
+            sns.violinplot(data=df.drop('target', axis=1), orient="h")
+            plt.title("Violin Plot de Todas as Features")
+            plt.xlabel("Valores das Features")
+            plt.tight_layout()
+            plt.show()
+            
         show_violin_plot()
     elif choice == '5':
+        def show_scatter_plot():
+            plt.figure(figsize=(12, 8))
+            sns.scatterplot(data=df, x='mean radius', y='mean texture', hue='target')
+            plt.title("Scatter Plot (Agrupado pelas Classes)")
+            plt.tight_layout()
+            plt.show()
+            
         show_scatter_plot()
     else:
         print("Opção inválida. Escolha novamente.")
